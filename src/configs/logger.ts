17e173bfe8ed2762ae8d6be80781eeec56a5bef7
nskumar278 <nskumar278@gmail.com>
2025-08-02 16:34:35 +0530

    Add logrotate for logs

--- src/configs/logger.ts
+++ src/configs/logger.ts
@@ -1,19 +1,20 @@
 import { createLogger, format, transports } from "winston";
+import DailyRotateFile from "winston-daily-rotate-file";
 
 const { combine, timestamp, printf, json, colorize } = format;
 
 const isProduction = process.env.NODE_ENV === 'production';
 
 const productionFormat = combine(
-  timestamp(),
-  json()
+    timestamp(),
+    json()
 );
 
 const developmentFormat = combine(
-  timestamp(),
-  printf(({ timestamp, level, message }) => {
-    return `${timestamp} [${level}]: ${message}`;
-  })
+    timestamp(),
+    printf(({ timestamp, level, message }) => {
+        return `${timestamp} [${level}]: ${message}`;
+    })
 );
 
 const logger = createLogger({
@@ -25,6 +26,21 @@ const logger = createLogger({
                 colorize(),
                 developmentFormat
             )
+        }),
+        new DailyRotateFile({
+            filename: 'logs/app-%DATE%.log',
+            datePattern: 'YYYY-MM-DD',
+            zippedArchive: true,
+            maxSize: '5m',
+            maxFiles: 3,
+        }),
+        new DailyRotateFile({
+            filename: "logs/error-%DATE%.log",
+            level: "error",
+            datePattern: "YYYY-MM-DD",
+            zippedArchive: true,
+            maxSize: "5m",
+            maxFiles: 3,
         }),
     ],
 });
